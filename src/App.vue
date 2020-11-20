82653796fd29805ec4e8a7b43d12408940bad6ff
anki2jain <jain2anki@gmail.com>
2020-11-20 12:05:25 +0530

    project initiated

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,23 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+
+    <dashboard></dashboard>
   </div>
 </template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
+  <div class="about text-center">
+    <input type="text" class="form-control" />
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style lang="scss"></style>
